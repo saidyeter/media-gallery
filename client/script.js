fb6235bdfb591da5cc0f3216cca40096c1617e04
Said Yeter <said.yeter@superentegrator.com>
2022-04-04 02:31:13 +0300

    Add filename below to image

--- client/script.js
+++ client/script.js
@@ -1,50 +1,7 @@
-
 const contentBox = document.querySelector("div.content");
 
 function getBaseUrl() {
-  return window.location.origin + window.location.pathname
-}
-
-function createLink({ Name, ThumbPath, ActualPath, IsDir }) {
-  const link = document.createElement("a");
-  link.setAttribute("alt", Name);
-  if (IsDir) {
-    const href = getBaseUrl() + "?directory=" + ActualPath
-    link.setAttribute("href", href);
-    link.style.backgroundImage = `url('folder.png')`;
-  } else {
-    link.setAttribute("href", "/file/" + ActualPath);
-    link.setAttribute("target", "_blank");
-    link.style.backgroundImage = `url('${"/file/" + ThumbPath}')`;
-  }
-  link.classList.add("photo");
-  link.classList.add("grid-item");
-  if (getContentState() == 'desc') {
-    link.style.display = 'none'
-  }
-  contentBox.appendChild(link);
-}
-
-function createDesc({ Name, ThumbPath, ActualPath, IsDir }) {
-  const div = document.createElement("div");
-  div.innerHTML = Name
-  let href
-  if (IsDir) {
-    href = getBaseUrl() + "?directory=" + ActualPath
-    div.style.backgroundImage = `url('folder.png')`;
-  } else {
-    href =  "/file/" + ActualPath
-    div.style.backgroundImage = `url('${"/file/" + ThumbPath}')`;
-  }
-
-  div.addEventListener("click", () => window.location = href);
-  div.classList.add("photo");
-  div.classList.add("desc");
-  div.classList.add("grid-item");
-  if (getContentState() == 'content') {
-    div.style.display = 'none'
-  }
-  contentBox.appendChild(div);
+  return window.location.origin + window.location.pathname;
 }
 
 function params() {
@@ -59,9 +16,8 @@ async function fillContent(address) {
   const data = await res.json();
   //  console.log(data);
   if (data.Contents && data.Contents.length > 0) {
-    data.Contents.forEach((content) => {
-      createLink(content);
-      createDesc(content);
+    data.Contents.forEach((content) => { 
+      createContent(content);
     });
 
     if (data.Next) {
@@ -93,40 +49,53 @@ function handleNext(folder, button) {
   address += "/" + folder;
 
   fillContent(address);
-  button.parentNode.removeChild(button)
+  button.parentNode.removeChild(button);
 }
 
 function createButton(url) {
   var button = document.createElement("button");
   button.type = "button";
-  button.innerText = "more"
+  button.innerText = "more";
   button.onclick = () => {
     handleNext(url, button);
   };
-  button.classList.add("grid-item")
+  button.classList.add("grid-item");
   contentBox.appendChild(button); // add the button to the context
 }
 
-function getContentState() {
-  const a = document.querySelector('a.photo')
-  if (a) {
-    return a.style.display == "none" ? 'desc' : 'content'
+function createContent({ Name, ThumbPath, ActualPath, IsDir }) {
+  // console.log("createContent", { Name, ThumbPath, ActualPath, IsDir });
+  const content = document.createElement("img");
+  content.onerror = () => {
+    content.src = "image-not-found.png";
+  } 
+   
+  content.setAttribute("alt", Name);
+  if (IsDir) {
+    content.setAttribute("src", 'folder.png');  
+  } else {
+    content.setAttribute("src", "/file/" + ThumbPath);
+    content.setAttribute("target", "_blank"); 
   }
-  return  'desc'
+  const contentWrapper = document.createElement('div')  
+  contentWrapper.classList.add('content-wrapper')
+
+  content.classList.add("photo");
+  content.classList.add("grid-item");
+  let href;
+  if (IsDir) {
+    href = getBaseUrl() + "?directory=" + ActualPath; 
+  } else {
+    href = "/file/" + ActualPath; 
+  }
+
+  contentWrapper.addEventListener("click", () => (window.location = href));
+  contentWrapper.style.cursor = "pointer";
+  contentWrapper.appendChild(content)
+
+  const contentName = document.createElement("div");
+  contentName.innerText = Name;
+  contentName.classList.add("content-name");
+  contentWrapper.appendChild(contentName);
+  contentBox.appendChild(contentWrapper);
 }
-function toggleDescAndLink() {
-  if (getContentState() == 'content') {
-    document.querySelectorAll('a.photo').forEach(link => link.style.display = 'none')
-    document.querySelectorAll('div.photo').forEach(link => link.style.display = 'flex')
-
-  }
-  else {
-    document.querySelectorAll('a.photo').forEach(link => link.style.display = 'block')
-    document.querySelectorAll('div.photo').forEach(link => link.style.display = 'none')
-
-  }
-}
-
-
-const toggle = document.querySelector("div.toggle");
-toggle.addEventListener('click', toggleDescAndLink)
